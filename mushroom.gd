0aa03bcccc5425ab9474175d75f55141cdc2f00a
wookbee <159189510+wookbee@users.noreply.github.com>
2025-09-14 21:24:46 +0100

    removed some unused stuff in mushroom.gd

--- mushroom.gd
+++ mushroom.gd
@@ -4,24 +4,25 @@ class_name Mushroom
 var growth := 0.1
 var grow_speed := 0.2
 var max_growth := 1.0
-var spawn_range := 0.3
-var max_family := 4
-@onready var mushroom_baby := load("res://mushroom.tscn")
-@onready var sprite :Sprite3D= $Sprite3D
 var grown := false
-var generation := 0
-var generational_loss := 0.2
-var family : Array[Mushroom]
-var parent : Mushroom
+
+var spawn_range := 0.3
 var grid: ForestGrid
-var spawn_height := 0.0
-var viewing := false
+
+@onready var mushroom_baby := load("res://mushroom.tscn")
+var max_family := 4
+var generation := 0 # current mushroom gen
+var generational_loss := 0.2 # shrinkage per generation
+var family : Array[Mushroom] # only the parent tracks this
+var parent : Mushroom
+
+@onready var sprite :Sprite3D= $Sprite3D
+var highlighted := false
 
 signal set_description(desc)
 
 func _ready() -> void:
 	$Area3D.input_event.connect(_on_area_input_event)
-	spawn_height = global_position.y
 	
 	if generation == 0:
 		parent = self
@@ -30,7 +31,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	_grow(delta)
 	
-	if viewing:
+	if highlighted:
 		var growth_percent := roundf((growth / max_growth) * 100)
 		var growth_colour : Color = lerp(Color.RED, Color.GREEN, growth_percent / 100.0)
 		var desc : String = "Mushroom gen " + str(generation) +  "\nFamily size: " + str(parent.family.size()) + "/" + str(max_family) + "\nGrowth: [color=#" + growth_colour.to_html() + "]" + str(growth_percent) + "%[/color]\nTile: " + grid.get_tile(global_position).type 
@@ -38,24 +39,20 @@ func _process(delta: float) -> void:
 
 func _grow(delta: float) -> void:
 	if growth >= max_growth:
-		growth = max_growth
 		if !grown and generation < max_family - 1:
-			_spread()
+			_spawn_mushroom()
 		return
 	
-	growth += delta * grow_speed
+	growth = move_toward(growth, max_growth, delta * grow_speed)
 	scale = Vector3.ONE * growth
 
 func _on_area_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int):
 	if event is InputEventMouseButton:
 		if event.is_pressed():
-			pass #_spread()
-
-func _spread() -> void:
-	_spawn_mushroom()
-	grown = true
+			pass # unused for now
 
 func _spawn_mushroom() -> void:
+	grown = true
 	var spawn_offset := Vector3(randf_range(-1, 1), 0.0, randf_range(-1, 1)).normalized() * spawn_range
 	var spawn_point := global_position + spawn_offset
 	
@@ -89,20 +86,15 @@ func _spawn_mushroom() -> void:
 	parent.family.append(new_mushroom)
 
 	new_mushroom.global_position = global_position + spawn_offset
-		
-	print("spawning mushroom on ", tile.type, ", gen: ", new_mushroom.generation)
-
-func _die() -> void:
-	pass
 
 func _on_area_3d_mouse_entered() -> void:
 	for mushroom in parent.family:
 		mushroom.sprite.shaded = false
 	
-	viewing = true
+	highlighted = true
 
 func _on_area_3d_mouse_exited() -> void:
 	for mushroom in parent.family:
 		mushroom.sprite.shaded = true
 	
-	viewing = false
+	highlighted = false
