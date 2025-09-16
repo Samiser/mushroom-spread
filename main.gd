356597c92b0e9429b5e13139622e96f6db4f09c1
wookbee <159189510+wookbee@users.noreply.github.com>
2025-09-16 17:31:06 +0100

    passive health loss

--- main.gd
+++ main.gd
@@ -30,7 +30,8 @@ func end_day() -> void:
 		if m is Mushroom and m.generation == 0:
 			var data: MushroomData = m.mushroom_data
 			var key := data.get_instance_id()
-			m.family_health += m.check_family_tiles()[0] * 2
+			m.family_health += m.check_family_tiles()[0] * 4
+			m.family_health -= m.family.size()
 			m.family_health = clampi(m.family_health, 0, 100)
 			if not seen.has(key):
 				data.max_family += 8
