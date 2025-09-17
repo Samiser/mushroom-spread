929259783b70078c765b6fe7ba0a74258fe28709
Samiser <github@me.samiser.xyz>
2025-09-17 14:52:43 +0000

    toggle hud button

--- hud.gd
+++ hud.gd
@@ -4,6 +4,7 @@ extends Control
 @export var tile_description_label: RichTextLabel
 @export var end_day_button: Button
 @export var start_day_button: Button
+@export var show_debug_button: Button
 
 signal day_ended
 signal day_started
@@ -24,7 +25,13 @@ func start_day() -> void:
 	start_day_button.visible = false
 	day_started.emit()
 
+func toggle_debug() -> void:
+	mushroom_description_label.visible = !mushroom_description_label.visible
+	tile_description_label.visible = !tile_description_label.visible
+
 func _ready() -> void:
 	end_day_button.visible = false
 	end_day_button.pressed.connect(end_day)
 	start_day_button.pressed.connect(start_day)
+	show_debug_button.pressed.connect(toggle_debug)
+	
