dd3a7e8114a587335a04a7936fb81118fb6ad7f8
Samiser <github@me.samiser.xyz>
2025-09-19 13:29:59 +0000

    added main menu

--- main_menu.gd
+++ main_menu.gd
@@ -2,3 +2,4 @@ extends Node3D
 
 func _ready() -> void:
 	$Environment.time_of_day = 0.8
+	$Control/VBoxContainer/Button.pressed.connect(func() -> void: get_tree().change_scene_to_file("res://main.tscn"))
