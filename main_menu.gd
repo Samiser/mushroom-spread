b31b6684bbea9891f774197b9fd87b1742dfaca4
wookbee <159189510+wookbee@users.noreply.github.com>
2025-09-19 22:26:39 +0100

    added tutorial option and made mouse pass-through tutorial boxes

--- main_menu.gd
+++ main_menu.gd
@@ -1,5 +1,16 @@
 extends Node3D
 
+var main_scene = preload("res://main.tscn").instantiate()
+@export var tutorial_button : Button
+@export var play_button : Button
+
 func _ready() -> void:
 	$Environment.time_of_day = 0.8
-	$Control/VBoxContainer/Button.pressed.connect(func() -> void: get_tree().change_scene_to_file("res://main.tscn"))
+	tutorial_button.pressed.connect(func() -> void: _load_main(true))
+	play_button.pressed.connect(func() -> void: _load_main(false))
+
+func _load_main(tutorials: bool) -> void:
+	main_scene.tutorials_enabled = tutorials
+	get_tree().root.add_child(main_scene)
+	get_node("/root/MainMenu").queue_free()
+	
